30b657abb531a5d35d5bedf43f5d86c65e153cfe
nillbit <nillbit2000@gmail.com>
2026-04-23 19:01:29 +0300

    added routes folder

--- router/userRoute.ts
+++ router/userRoute.ts
@@ -2,6 +2,6 @@ import { Request, Response } from "express";
 import router from "./route";
 
 router.get("/testing",(req:Request,res:Response)=>{
-    res.json({message:"it worked nicely"})
+    res.json({message:"Happy coding!"})
 })
 export default router;
